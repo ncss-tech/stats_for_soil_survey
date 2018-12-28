14bbfee5b7ee57660f045418f91c5733ce787151
Skye Wills <skye.wills@lin.usda.gov>
2018-12-28 11:09:05 -0600

    Add Getting started stuff

---
+++ data/Pre-course/R_sand/sand_ggplot.R
@@ -0,0 +1,17 @@
+# this is a demo of how to use R editor with an R Commander Script
+
+sand2 <- read.table("C:/R_data/sand_example.csv", header=TRUE, sep=",", 
+  na.strings="NA", dec=".", strip.white=TRUE)
+library(relimp, pos=4)
+showData(sand2, placement='-20+200', font=getRcmdr('logFont'), maxwidth=80, 
+  maxheight=30)
+Hist(sand2$sand, scale="frequency", breaks="Sturges", col="blue")
+
+Boxplot(sand~landuse, data=sand2, id.method="y")
+summary(sand2)
+library(multcomp, pos=4)
+library(abind, pos=4)
+AnovaModel.1 <- aov(sand ~ landuse, data=sand2)
+summary(AnovaModel.1)
+numSummary(sand2$sand , groups=sand2$landuse, statistics=c("mean", "sd"))
+
