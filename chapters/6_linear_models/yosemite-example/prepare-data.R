95c947002949d9478eb9df538765a295eff23bbc
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-03-09 16:55:12 -0700

    moving things around, adding yosemite example

---
+++ chapters/6_linear_models/yosemite-example/prepare-data.R
@@ -0,0 +1,50 @@
+library(raster)
+library(soilDB)
+library(rgdal)
+library(sp)
+
+# latest data from Henry
+# this includes reasonable estimates of MAST, MSST, MWST
+m <- fetchHenry(what='all', project='CA790')
+m <- m$sensors
+
+# just sensors at 50cm
+m <- subset(m, subset=sensor_depth == 50)
+
+# load extra site information
+site.data <- read.csv('site-data.csv', stringsAsFactors=FALSE)
+
+# create site_name by adding yosemite_ to each id
+site.data$name <- paste0('yosemite_', site.data$id)
+
+# read in elevation and solar radiation data
+r.elev <- raster('L:/NRCS/MLRAShared/CA790/DEM-derived/elev30.tif')
+r.solar <- raster('L:/NRCS/MLRAShared/CA790/DEM-derived/beam_rad_sum_mj.tif')
+r.tci <- raster('L:/NRCS/MLRAShared/CA790/DEM-derived/saga-wetness.tif')
+
+# combine into a stack, and fix names
+r.stack <- stack(r.elev, r.solar, r.tci)
+names(r.stack) <- c('elev','solar', 'tci')
+
+# transform hobo locations to CRS of raster
+m <- spTransform(m, CRS(projection(r.stack)))
+
+# extract raster stack at hobo locations
+e <- extract(r.stack, m)
+
+# convert to DF and add site_name
+e <- data.frame(name=m$name, e, stringsAsFactors = FALSE)
+
+# join with spatial data + MAST/MSST/MWST
+m <- merge(m, e, by='name')
+
+# join O hz thickness
+m <- merge(m, site.data[, c('name', 'o.hz.thick')], by='name')
+
+# keep SPDF and re-name
+s <- m
+
+# save for later use
+save(s, file='cached-data.rda')
+
+
