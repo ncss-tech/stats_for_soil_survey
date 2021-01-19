6b490ac362e3236a56df4323614a8590e1bd8724
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-01-19 15:04:25 -0800

    Notes RE: building book / interactive maps with mapview / html_dependency

--- newbook/00-book-setup.R
+++ newbook/00-book-setup.R
@@ -1,4 +1,10 @@
+## get bookdown
 # install.packages('bookdown')
+
+## for proper html_dependency (CSS files) need >2.9.0
+## or the interactive plots will not render in bookdown
+## (note: they should work fine in regular e.g. Rstudio sessions)
+# remotes::install_github('r-spatial/mapview')
 
 options(bookdown.clean_book = TRUE)
 
