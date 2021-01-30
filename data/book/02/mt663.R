a9be801499f1ee06a8bf40de93a90ff2ed3bacbf
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-01-29 22:56:55 -0800

    Update ZIP (add default soil_color)

--- data/book/02/mt663.R
+++ data/book/02/mt663.R
@@ -7,5 +7,5 @@ mt663ext <- get_extended_data_from_NASIS_db()
 
 soilDB.env
 
-save(mt663, mt663ext, soilDB.env, file =  "data/book/02/mt663.rda")
+save(mt663, mt663ext, soilDB.env, file =  "../data/book/02/mt663.rda")
 zip("data/book/02/mt663.zip", "data/book/02/mt663.rda")
