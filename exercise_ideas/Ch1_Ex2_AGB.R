0d0106187233f954c79d3fcc39e7008e6b098955
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-02-01 21:52:02 -0800

    Remove redundant syntax + better comments

--- exercise_ideas/Ch1_Ex2_AGB.R
+++ exercise_ideas/Ch1_Ex2_AGB.R
@@ -19,34 +19,33 @@ mu <- get_mapunit_from_NASISWebReport(areasymbol = area_symbols)
 
 # CALCULATE number of hydric acres in each dmuiid
 # note: pct_hydric contains whole number values, need to convert to proportion divide by 100
-dmuiid_hydric <-  mutate(mu, hydric_acres = muacres * pct_hydric /  100) %>%
-  filter(hydric_acres > 0) 
+dmuiid_hydric <-  mutate(mu, hydric_acres = muacres * pct_hydric /  100)
 
 # CALCULATE TOTAL hydric acres by areasymbol
 hydric_by_area <- dmuiid_hydric %>%
   group_by(areasymbol) %>% 
   summarize(total_hydric = sum(hydric_acres, na.rm = TRUE))
 
-# left join FY2021 approved project mapunit TO hydric mapunits, 
+# LEFT JOIN FY2021 approved project mapunit TO hydric mapunits, 
 #   group by areasymbol, sum hydric acres by areasymbol (just project mapunits!!!)
-hydric_in_projects <- filter(prjmu, dmuiid %in% dmuiid_hydric$dmuiid) %>%
-  left_join(dmuiid_hydric) %>%
+hydric_in_projects <- left_join(prjmu, dmuiid_hydric) %>%
   group_by(areasymbol) %>%
   summarize(project_hydric = sum(hydric_acres, na.rm = TRUE))
 
-# use purrr::map_df to supply several areasymbols to 
+# USE purrr::map_df to supply several areasymbols to 
 #   get_legend_from_NASISWebReport and return combined data.frame
-leg <- purrr::map_df(area_symbols, function(x)
-  get_legend_from_NASISWebReport(areasymbol = x, mlraoffice = "%"))
+leg <- purrr::map_df(area_symbols, function(x) {
+  get_legend_from_NASISWebReport(areasymbol = x, mlraoffice = "%")
+})
 
-# join legends to hydric by area and hydric by project
-#   calculate the % of total area that is hydric
-res <- leg %>% 
-  left_join(hydric_by_area, by = "areasymbol") %>% 
+# LEFT JOIN legend to hydric by area and hydric by project
+#   then CALCULATE the % of total area that is hydric, AND % in projects 2021 that is hydric
+res <- left_join(leg, hydric_by_area, by = "areasymbol") %>%
   left_join(hydric_in_projects, by = "areasymbol") %>%
-  mutate(across(total_hydric:project_hydric, 
-                function(x) x / areaacres * 100, 
-                .names = "{.col}_pct"))
+  mutate(across(total_hydric:project_hydric,
+           function(x) x / areaacres * 100,
+           .names = "{.col}_pct"))
 
+# display selected columns and summaries
 select(res, areasymbol, ssastatus, areaacres, n_lmapunitiid, total_hydric:project_hydric_pct) %>%
   arrange(desc(total_hydric_pct))
