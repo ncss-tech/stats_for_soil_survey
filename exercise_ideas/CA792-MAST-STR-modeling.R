7a06c2d14f2c1eb5b8770beec22d2ae6386a3fbd
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-05-11 00:40:43 -0700

    edits

--- exercise_ideas/CA792-MAST-STR-modeling.R
+++ exercise_ideas/CA792-MAST-STR-modeling.R
@@ -1,4 +1,56 @@
 library(aqp)
+library(e1071)
+library(lattice)
+library(tactile)
+
+# get data
+tf <- tempfile()
+download.file('https://github.com/ncss-tech/stats_for_soil_survey/raw/master/data/CA792-MAST-STR-modeling.rds', tf)
+
+# load from .RDS
+x <- readRDS(tf)
+
+# check
+str(x)
+
+# Shannon entropy for STR predictions
+x$H <- apply(x[, c('Pr_cryic', 'Pr_frigid', 'Pr_mesic', 'Pr_thermic')], 1, shannonEntropy)
+
+bwplot(STR ~ H, data = x, par.settings = tactile.theme(), xlab = 'Shannon Entropy (base 2)')
 
 
-x <- readRDS('../data/CA792-MAST-STR-modeling.rds')
+head(x)
+
+
+## PCC / Kappa
+
+(tab <- table(observed = x$STR, predicted = x$pred_STR))
+classAgreement(tab, match.names = TRUE)
+
+
+## Tau
+
+# weight matrix...
+
+# prior proportions
+priors <-  apply(tab, 2, sum)/sum(tab)
+
+# see ?tauW
+tauW(tab, P = priors)
+
+
+
+
+## Brier Scores
+
+# prepare input data
+B <- data.frame(
+  actual = x$STR,
+  x[, c('Pr_cryic', 'Pr_frigid', 'Pr_mesic', 'Pr_thermic')]
+)
+
+names(B)[2:5] <- levels(x$STR)
+
+# compute Brier scores, lower is better
+brierScore(B, classLabels = levels(x$STR), actual = 'actual')
+
--- exercise_ideas/mukind-entropy-1.R
+++ exercise_ideas/mukind-entropy-1.R
@@ -16,7 +16,13 @@ shannonEntropy(x)
 x <- c(85, 10, 5) / 100
 shannonEntropy(x)
 
+# 4, equal prob
+x <- rep(1, times = 4) / 4
+shannonEntropy(x)
 
+# 5, equal prob
+x <- rep(1, times = 5) / 5
+shannonEntropy(x)
 
 
 
@@ -58,6 +64,11 @@ head(x)
 
 x$mukind <- factor(x$mukind, levels = c("Association", "Consociation", "Complex", "Undifferentiated group"))
 
+
+# ~ median of 4 components / MU
+summary(tapply(x$mukind, x$mukey, length))
+
+
 xx <- split(x, x$mukey)
 
 xx[[1]]
@@ -66,9 +77,10 @@ z <- lapply(xx, MU_entropy)
 z <- do.call('rbind', z)
 
 bwplot(areasymbol ~ H, data = z, par.settings = tactile.theme(), varwidth = TRUE)
+bwplot(mukind ~ H, data = z, par.settings = tactile.theme(), varwidth = TRUE)
 
-bwplot(mukind ~ H | areasymbol, data = z, par.settings = tactile.theme(), varwidth = TRUE)
-
+bwplot(mukind ~ H | areasymbol, data = z, par.settings = tactile.theme(), varwidth = TRUE, as.table = TRUE)
+bwplot(areasymbol ~ H | mukind, data = z, par.settings = tactile.theme(), varwidth = TRUE, as.table = TRUE)
 
 
 
