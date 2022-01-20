4243b000804ee99b72ad49dc8e47383d7a9fd8a2
Andrew Brown <andrew.g.brown@usda.gov>
2022-01-20 10:47:04 -0800

    MT647: use rmHzErrors=TRUE for QC output

--- data/book/02/mt647.R
+++ data/book/02/mt647.R
@@ -6,5 +6,5 @@ library(soilDB)
 #  NASIS Site: MLRA04%
 #  NASIS Group: 4-MIS Pedons
 
-mt647 <- fetchNASIS(rmHzErrors = FALSE)
+mt647 <- fetchNASIS()
 save(mt647, soilDB.env, file =  "../data/book/02/mt647.rda")
