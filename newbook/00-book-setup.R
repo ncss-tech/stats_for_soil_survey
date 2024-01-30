428cb4268e351ab89ec813dbea43028c7da035da
Andrew Gene Brown <andrew.g.brown@usda.gov>
2024-01-29 20:53:45 -0800

    bump book

--- newbook/00-book-setup.R
+++ newbook/00-book-setup.R
@@ -1,11 +1,6 @@
 ## get bookdown
 # install.packages('bookdown')
 
-## for proper html_dependency (CSS files) need >2.9.0
-## or the interactive plots will not render in bookdown
-## (note: they should work fine in regular e.g. Rstudio sessions)
-# remotes::install_github('r-spatial/mapview')
-
 options(bookdown.clean_book = TRUE)
 
 # remove ../book
