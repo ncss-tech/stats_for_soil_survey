141a6d06ecdb9b92d97b55f68b7e52386d22e075
Andrew Gene Brown <andrew.g.brown@usda.gov>
2026-01-16 16:11:05 -0800

    build: updating precourse/package config

--- Part2/00-book-setup.R
+++ Part2/00-book-setup.R
@@ -1,13 +1,1 @@
-## get bookdown
-# install.packages('bookdown')
-
-options(bookdown.clean_book = TRUE)
-
-# remove ../book
-bookdown::clean_book()
-unlink("s4ssbook.rds")
-
-# create ../book2
-bookdown::render_book(".")
-
-
+source("../Part1/00-book-setup.R")
